e32581c425a85c1b59f0cdf45c196fefebee1ae1
le-anne <leannevictoriaharris@gmail.com>
2025-01-22 02:21:34 -0800

    updated configs

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
-export const SITE_TITLE = "Cassidy's blog template";
-export const SITE_DESCRIPTION = "A blog template for you!";
+export const SITE_TITLE = "In Lieu of Therapy";
+export const SITE_DESCRIPTION = "";
